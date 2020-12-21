ab3565860b2cb3eec6a0f1daf394362c0608e78a
abellusc <alexandra.belluscio@icloud.com>
2020-12-21 15:10:34 -0500

    feat(milestone): initial commit

---
+++ src/characters/BaseCharacter.ts
@@ -0,0 +1,6 @@
+import { Creature } from './Creature';
+import { NPC } from './NPC';
+
+export abstract class BaseCharacter extends Creature implements NPC {
+
+}
---
+++ src/characters/Creature.ts
@@ -0,0 +1,3 @@
+export abstract class Creature extends EventEmitter {
+    
+}
